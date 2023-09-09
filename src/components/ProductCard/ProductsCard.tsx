38826b8407120619e065b86a6b09eea200bda384
Facu-J-Boy <boyfacundo45@gmail.com>
2023-09-09 19:58:12 -0300

    feat: cart icon in product card

--- src/components/ProductCard/ProductsCard.tsx
+++ src/components/ProductCard/ProductsCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './ProductCard.module.css';
 import { Link } from 'react-router-dom';
+import { GrCart } from 'react-icons/gr';
 
 interface ProductCardProps {
   id: number;
@@ -19,6 +20,16 @@ const ProductsCard: React.FC<ProductCardProps> = ({
     <div className={styles.product}>
       <Link to={`/product/${id}`} className={styles.cardLink}>
         <div className={styles.imageContainer}>
+          <div className={styles.cart_icon}>
+            <GrCart
+              size={30}
+              style={{
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                padding: '5px',
+                borderRadius: '10px'
+              }}
+            />
+          </div>
           <img src={image} alt={title} />
         </div>
         <h1>{`$${price}`}</h1>
