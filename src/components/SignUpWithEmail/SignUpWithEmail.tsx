8976c4d6f34f3b961859b4292dd1916eee00a821
Facu-J-Boy <boyfacundo45@gmail.com>
2023-09-13 17:45:56 -0300

    feat: loading when sign up

--- src/components/SignUpWithEmail/SignUpWithEmail.tsx
+++ src/components/SignUpWithEmail/SignUpWithEmail.tsx
@@ -1,22 +1,28 @@
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const SignUpWithEmail: React.FC = (): JSX.Element => {
   const { register, handleSubmit } = useForm();
 
+  const [loading, setLoading] = useState(false);
+
   const onSubmit = async (data: any) => {
+    setLoading(true); // Comienza  a ejecutarse la funcion
     const { email, password } = data;
     try {
       const auth = getAuth();
       await createUserWithEmailAndPassword(auth, email, password);
+      setLoading(false); // Si el usuario se logueó correctamente termina el loading
     } catch (error) {
+      setLoading(false); // Si hay un error también
       console.error('Error: ', error);
       // Maneja el error de creación de usuario según tus necesidades
     }
   };
   return (
     <div>
+      {loading === true ? <p>loading...</p> : null}
       <h2>Registrarse</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>
