99452e672611920b84628fb252ec1f8abb0dc6a8
Facu-J-Boy <boyfacundo45@gmail.com>
2023-09-20 18:04:59 -0300

    feat: validations in login form

--- src/components/LoginWhitEmail/LoginWhitEmail.tsx
+++ src/components/LoginWhitEmail/LoginWhitEmail.tsx
@@ -4,11 +4,16 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import LoginWithGoogle from '../LoginWithGoogle/LoginWithGoogle';
 import { Link } from 'react-router-dom';
 import styles from './LoginWithEmail.module.css';
+import { FormData } from '../../interfaces/formData';
 
 const LoginWhitEmail: React.FC = (): JSX.Element => {
   const [loading, setLoading] = useState<boolean>(false);
 
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>();
 
   const onSubmit = async (data: any) => {
     setLoading(true); // Comienza  a ejecutarse la funcion
@@ -36,19 +41,39 @@ const LoginWhitEmail: React.FC = (): JSX.Element => {
         </div>
         <h2 style={{ color: '#333' }}> Sign in to your account</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label>Your e-mail:</label>
-          <input
-            type='email'
-            placeholder='name@company.com'
-            {...register('email')}
-          />
+          <label>
+            Your e-mail:
+            <input
+              style={errors.email && { borderColor: 'red' }}
+              placeholder='name@company.com'
+              {...register('email', {
+                required: { value: true, message: 'e-mail is required' }, // Si no hay nada escrito en el input de email se coloca un mensaje
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, // Si en el input no se cumple con esta expreción regular se coloca un mensaje distinto
+                  message: 'Invalid email'
+                }
+              })}
+            />
+            {errors.email && (
+              <span className={styles.error}>{errors.email.message}</span> // Si hay un error en el registro de usuario se muestra el mensaje en un span
+            )}
+          </label>
           <br />
-          <label>Password:</label>
-          <input
-            type='password'
-            placeholder='••••••••'
-            {...register('password')}
-          />
+          <label>
+            Password:
+            <input
+              style={errors.password && { borderColor: 'red' }}
+              type='password'
+              placeholder='••••••••'
+              {...register('password', {
+                required: { value: true, message: 'Password is required' },
+                minLength: { value: 6, message: 'Min 6 character' }
+              })}
+            />
+            {errors.password && (
+              <span className={styles.error}>{errors.password.message}</span>
+            )}
+          </label>
           <br />
           <button className={styles.submit} type='submit'>
             Sign in
