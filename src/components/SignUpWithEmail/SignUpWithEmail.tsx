f533a6025299df44ef78e7dbb43e5a24c631000e
Facu-J-Boy <boyfacundo45@gmail.com>
2023-09-20 17:41:02 -0300

    feat: validations in signUp form

--- src/components/SignUpWithEmail/SignUpWithEmail.tsx
+++ src/components/SignUpWithEmail/SignUpWithEmail.tsx
@@ -3,8 +3,19 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './SignUpWithEmail.module.css';
 
+interface FormData {
+  email: string;
+  password: string;
+}
+
 const SignUpWithEmail: React.FC = (): JSX.Element => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>();
+
+  console.log('errors:', errors);
 
   const [loading, setLoading] = useState(false);
 
@@ -37,10 +48,18 @@ const SignUpWithEmail: React.FC = (): JSX.Element => {
           <label>
             E-mail:
             <input
-              type='email'
               placeholder='name@company.com'
-              {...register('email')}
+              {...register('email', {
+                required: { value: true, message: 'e-mail is required' }, // Si no hay nada escrito en el input de email se coloca un mensaje
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, // Si en el input no se cumple con esta expreción regular se coloca un mensaje distinto
+                  message: 'Invalid email'
+                }
+              })}
             />
+            {errors.email && (
+              <span className={styles.error}>{errors.email.message}</span> // Si hay un error en el registro de usuario se muestra el mensaje en un span
+            )}
           </label>
           <br />
           <label>
@@ -48,8 +67,14 @@ const SignUpWithEmail: React.FC = (): JSX.Element => {
             <input
               type='password'
               placeholder='••••••••'
-              {...register('password')}
+              {...register('password', {
+                required: { value: true, message: 'Password is required' },
+                minLength: { value: 6, message: 'Min 6 character' }
+              })}
             />
+            {errors.password && (
+              <span className={styles.error}>{errors.password.message}</span>
+            )}
           </label>
           <br />
           <button className={styles.submit} type='submit'>
