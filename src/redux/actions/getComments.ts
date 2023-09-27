8982f47a73aa9773e417b88d431e88d7ae5a6d5a
Facu-J-Boy <boyfacundo45@gmail.com>
2023-09-27 16:15:02 -0300

    fix: getComments settings

--- src/redux/actions/getComments.ts
+++ src/redux/actions/getComments.ts
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getComments = createAsyncThunk('getComments', async () => {
+export const getComments = createAsyncThunk('getComments', async (id) => {
   try {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts/1/comments'
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`
     );
     return response.data;
   } catch (error) {
