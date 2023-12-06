c0c3f203ea9ce6da1f71f01c53862076727284ef
Facu-J-Boy <boyfacundo45@gmail.com>
2023-12-06 19:35:28 -0300

    feat: when the user is not login in admin dashboard the page is not found

--- src/pages/AdminDashboard/index.tsx
+++ src/pages/AdminDashboard/index.tsx
@@ -8,9 +8,26 @@ import DashboardProducts from '../../components/DashboardProducts/DashboardProdu
 import Loader from '../../components/Loader/Loader';
 import { getAllCategories } from '../../redux/actions/getAllCategories';
 import Categories from '../../components/Categories/Categories';
+import { auth } from '../../Firebase';
+import { useNavigate } from 'react-router-dom';
 
 const AdminDashboard: React.FC = (): JSX.Element => {
   const [activeTab, setActiveTab] = useState('home');
+
+  const [login, setLogin] = useState<boolean | string>('pending');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setLogin(true);
+      } else {
+        setLogin(false);
+        navigate('*');
+      }
+    });
+  }, [navigate]);
 
   const handleTabClick = (tabId: string) => {
     setActiveTab(tabId);
@@ -32,129 +49,112 @@ const AdminDashboard: React.FC = (): JSX.Element => {
   }, [dispatch]);
 
   return (
-    <div className={styles.container}>
-      <ul className={styles.myTab} role='tablist'>
-        <li className={styles.navItem}>
-          <button
-            className={`${styles.navLink} ${
-              activeTab === 'home' && styles.active
-            }`}
-            onClick={() => handleTabClick('home')}
-            type='button'
-            role='tab'
-            aria-controls='home-tab-pane'
-            aria-selected={activeTab === 'home'}
-          >
-            Products
-          </button>
-        </li>
-        <li className={styles.navItem}>
-          <button
-            className={`${styles.navLink} ${
-              activeTab === 'profile' && styles.active
-            }`}
-            onClick={() => handleTabClick('profile')}
-            type='button'
-            role='tab'
-            aria-controls='profile-tab-pane'
-            aria-selected={activeTab === 'profile'}
-          >
-            Categories
-          </button>
-        </li>
-        <li className={styles.navItem}>
-          <button
-            className={`${styles.navLink} ${
-              activeTab === 'contact' && styles.active
-            }`}
-            onClick={() => handleTabClick('contact')}
-            type='button'
-            role='tab'
-            aria-controls='contact-tab-pane'
-            aria-selected={activeTab === 'contact'}
-          >
-            Contact
-          </button>
-        </li>
-        {/* <li className={styles.navItem}>
-          <button
-            className={`${styles.navLink} ${
-              activeTab === 'disabled' && styles.active
-            } ${styles.disabled}`}
-            onClick={() => handleTabClick('disabled')}
-            type='button'
-            role='tab'
-            aria-controls='disabled-tab-pane'
-            aria-selected={activeTab === 'disabled'}
-            disabled
-          >
-            Disabled
-          </button>
-        </li> */}
-      </ul>
-      <div className={styles.myTabContent}>
-        <div
-          className={`${styles.tabPane} ${
-            activeTab === 'home' && styles.active
-          }`}
-          role='tabpanel'
-        >
-          {productsLoading ? (
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <Loader />
+    <>
+      {login === 'pending'
+        ? null
+        : login === true && (
+            <div className={styles.container}>
+              <ul className={styles.myTab} role='tablist'>
+                <li className={styles.navItem}>
+                  <button
+                    className={`${styles.navLink} ${
+                      activeTab === 'home' && styles.active
+                    }`}
+                    onClick={() => handleTabClick('home')}
+                    type='button'
+                    role='tab'
+                    aria-controls='home-tab-pane'
+                    aria-selected={activeTab === 'home'}
+                  >
+                    Products
+                  </button>
+                </li>
+                <li className={styles.navItem}>
+                  <button
+                    className={`${styles.navLink} ${
+                      activeTab === 'profile' && styles.active
+                    }`}
+                    onClick={() => handleTabClick('profile')}
+                    type='button'
+                    role='tab'
+                    aria-controls='profile-tab-pane'
+                    aria-selected={activeTab === 'profile'}
+                  >
+                    Categories
+                  </button>
+                </li>
+                <li className={styles.navItem}>
+                  <button
+                    className={`${styles.navLink} ${
+                      activeTab === 'contact' && styles.active
+                    }`}
+                    onClick={() => handleTabClick('contact')}
+                    type='button'
+                    role='tab'
+                    aria-controls='contact-tab-pane'
+                    aria-selected={activeTab === 'contact'}
+                  >
+                    Contact
+                  </button>
+                </li>
+              </ul>
+              <div className={styles.myTabContent}>
+                <div
+                  className={`${styles.tabPane} ${
+                    activeTab === 'home' && styles.active
+                  }`}
+                  role='tabpanel'
+                >
+                  {productsLoading ? (
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                      <Loader />
+                    </div>
+                  ) : (
+                    <>
+                      {products.map((p: product) => (
+                        <DashboardProducts
+                          key={p.id}
+                          id={p.id}
+                          image={p.image}
+                          title={p.title}
+                          price={p.price}
+                        />
+                      ))}
+                    </>
+                  )}
+                </div>
+                <div
+                  className={`${styles.tabPane} ${
+                    activeTab === 'profile' && styles.active
+                  }`}
+                  role='tabpanel'
+                >
+                  {categoriesLoading ? (
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                      <Loader />
+                    </div>
+                  ) : (
+                    <>
+                      {categories.map(
+                        (c: string, index: React.Key | null | undefined) => (
+                          <Categories key={index} categorie={c} />
+                        )
+                      )}
+                    </>
+                  )}
+                </div>
+                <div
+                  className={`${styles.tabPane} ${
+                    activeTab === 'contact' && styles.active
+                  }`}
+                  role='tabpanel'
+                >
+                  Contact Content
+                </div>
+              </div>
             </div>
-          ) : (
-            <>
-              {products.map((p: product) => (
-                <DashboardProducts
-                  key={p.id}
-                  id={p.id}
-                  image={p.image}
-                  title={p.title}
-                  price={p.price}
-                />
-              ))}
-            </>
           )}
-        </div>
-        <div
-          className={`${styles.tabPane} ${
-            activeTab === 'profile' && styles.active
-          }`}
-          role='tabpanel'
-        >
-          {categoriesLoading ? (
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <Loader />
-            </div>
-          ) : (
-            <>
-              {categories.map(
-                (c: string, index: React.Key | null | undefined) => (
-                  <Categories key={index} categorie={c} />
-                )
-              )}
-            </>
-          )}
-        </div>
-        <div
-          className={`${styles.tabPane} ${
-            activeTab === 'contact' && styles.active
-          }`}
-          role='tabpanel'
-        >
-          Contact Content
-        </div>
-        {/* <div
-          className={`${styles.tabPane} ${
-            activeTab === 'disabled' && styles.active
-          }`}
-          role='tabpanel'
-        >
-          Disabled Content
-        </div> */}
-      </div>
-    </div>
+    </>
   );
 };
 
