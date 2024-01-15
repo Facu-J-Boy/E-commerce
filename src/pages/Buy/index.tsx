bb9be4558659697a554975486ef1c0cc2bc5544e
Facu-J-Boy <boyfacundo45@gmail.com>
2024-01-15 19:07:32 -0300

    feat: buy styles beta

--- src/pages/Buy/index.tsx
+++ src/pages/Buy/index.tsx
@@ -6,6 +6,8 @@ import { AppDispatch } from '../../redux/store';
 import { getSingleProduct } from '../../redux/actions/getSingleProduct';
 import ProductItem from '../../components/NavBar/ProductItem/ProductItem';
 import { product } from '../../interfaces/product';
+import { clearProductState } from '../../redux/actions/clearProductState';
+import { FaArrowLeftLong } from 'react-icons/fa6';
 
 const Buy: React.FC = (): JSX.Element => {
   const { id } = useParams();
@@ -22,38 +24,69 @@ const Buy: React.FC = (): JSX.Element => {
 
   const [items, setItems] = useState<product[]>([]);
 
+  console.log('items: ', items);
+
+  useEffect(() => {
+    id !== 'cart' && setItems([product]);
+  }, [product, id]);
+
   useEffect(() => {
     if (id === 'cart') {
-      product && setItems(cartProducts);
+      setItems(cartProducts);
     } else {
       dispatch(getSingleProduct(id));
-      product && setItems([product]);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => {
+      dispatch(clearProductState());
+    };
   }, [dispatch, id, cartProducts]);
 
   return (
-    <div className={styles.container}>
+    <>
       {productLoading ? (
         <p>loading...</p>
       ) : (
-        <ul className={styles.productList}>
-          {items.map((element: any) => (
-            <ol>
-              <ProductItem
-                id={element.id}
-                title={element.title}
-                price={element.price}
-                image={element.image}
-              />
-            </ol>
-          ))}
-        </ul>
+        <div className={styles.container}>
+          <div className={styles.leftZone}>
+            <div className={styles.title}>
+              <h1>Shopping Cart</h1>
+              <h1>{`${items.length} items`}</h1>
+            </div>
+            <ul className={styles.productList}>
+              {items.map((element: any) => (
+                <ol>
+                  <ProductItem
+                    id={element.id}
+                    title={element.title}
+                    price={element.price}
+                    image={element.image}
+                  />
+                </ol>
+              ))}
+            </ul>
+            <button>
+              <FaArrowLeftLong style={{ marginRight: '10px' }} />
+              Continue Shopping
+            </button>
+          </div>
+          <div className={styles.rightZone}>
+            <h4>Order Summary</h4>
+            <div className={styles.form}>
+              <h4>SHIPPING</h4>
+              <input type='text' />
+              <h4>PROMO CODE</h4>
+              <input type='text' />
+              <button>APPLY</button>
+            </div>
+            <div className={styles.total}>
+              <h3>Total: </h3>
+              <h3>{`$${id === 'cart' ? total : product.price}`}</h3>
+            </div>
+            <button>CHECKOUT</button>
+          </div>
+        </div>
       )}
-      <div className={styles.form}>
-        <h1>{`${items.length} items`}</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
